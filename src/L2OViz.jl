6a4a8d4542075bbb1ab85d7b4d56c8dd64d9cf20
Ivan <gqiu7@gatech.edu>
2026-06-02 13:29:03 -0400

    Update file names

--- src/L2OViz.jl
+++ src/L2OViz.jl
@@ -5,9 +5,9 @@ using CairoMakie
 include("select_entries.jl")
 include("_utils.jl")
 include("plot_variable.jl")
-include("plot_matrix_variable.jl")
+include("plot_graph_variable.jl")
 include("animate_variable.jl")
-include("animate_matrix_variable.jl")
+include("animate_graph_variable.jl")
 
 export plot_variable, plot_graph_variable, animate_variable, animate_graph_variable
 
